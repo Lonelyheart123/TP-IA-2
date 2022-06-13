85cbfb06408c3a20692478ac57476de3cebe9854
Lonelyheart123 <perezortiztomas@gmail.com>
2022-06-13 15:21:40 -0300

    Tree vuelto a como estaba antes

--- TP IA Steering/Assets/Scripts/Enemy/EnemyChase.cs
+++ TP IA Steering/Assets/Scripts/Enemy/EnemyChase.cs
@@ -2,68 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyChase<T> : States<T>
+namespace EnemyStates
 {
-    GameObject Player;
-    public bool _canPatrol = true;
-
-    EnemyController enemyController;
-    Transform _target;
-    Enemy _enemy;
-    public bool inSight;
-    float _distance = 0;
-
-    Transform _npc;
-    INode _root;
-
-    public EnemyChase(Enemy enemyModel, Transform target, float distance, Transform npc, INode root)
+    public class EnemyChase<T> : States<T>
     {
-        _target = target;
-        _enemy = enemyModel;
-        _distance = distance;
-        _npc = npc;
-        _root = root;
-    }
+        GameObject Player;
+        public bool _canPatrol = true;
 
-    public override void Init()
-    {
-        Debug.Log("Awake");
-    }
+        EnemyController enemyController;
+        Transform _target;
+        Enemy _enemy;
+        public bool inSight;
+        float _distance = 0;
 
-    public Vector3 GetDir()
-    {
-        //PlayerDetected = true;
-        Vector3 dir = (_target.position - _npc.position).normalized;
-        return dir;
-    }
+        Transform _npc;
+        INode _root;
 
-    void MoveToPlayer()
-    {
-        if (enemyController.LineOfSight() == true && enemyController.ShootRange() == false)
+        public EnemyChase(Enemy enemyModel, Transform target, float distance, Transform npc, INode root)
         {
-            Vector3 dir = GetDir();
-            _enemy.transform.LookAt(_target.position);
-            var ySpeed = _enemy.GetComponent<Rigidbody>().velocity.y;
-            _enemy.GetComponent<Rigidbody>().velocity = new Vector3(dir.x * _enemy.speed, ySpeed, dir.z * _enemy.speed);
+            _target = target;
+            _enemy = enemyModel;
+            _distance = distance;
+            _npc = npc;
+            _root = root;
         }
-        else if (enemyController.LineOfSight() == true && enemyController.ShootRange() == true)
-        {
-            _root.execute();
-        }
-        else if (enemyController.LineOfSight() == false && enemyController.ShootRange() == false)
-        {
-            _root.execute();
-        }
-    }
-    
-    public override void Execute()
-    {
-        MoveToPlayer();
-        Debug.Log("Chasing"); 
-    }
 
-    public override void Exit()
-    {
-        Debug.Log("Enemy ChaseState sleep");
+        public override void Init()
+        {
+            Debug.Log("Awake");
+        }
+
+        public Vector3 GetDir()
+        {
+            //PlayerDetected = true;
+            Vector3 dir = (_target.position - _npc.position).normalized;
+            return dir;
+        }
+
+        void MoveToPlayer()
+        {
+            if (enemyController.LineOfSight() == true && enemyController.ShootRange() == false)
+            {
+                Vector3 dir = GetDir();
+                _enemy.transform.LookAt(_target.position);
+                var ySpeed = _enemy.GetComponent<Rigidbody>().velocity.y;
+                _enemy.GetComponent<Rigidbody>().velocity = new Vector3(dir.x * _enemy.speed, ySpeed, dir.z * _enemy.speed);
+            }
+            else if (enemyController.LineOfSight() == true && enemyController.ShootRange() == true)
+            {
+                _root.execute();
+            }
+            else if (enemyController.LineOfSight() == false && enemyController.ShootRange() == false)
+            {
+                _root.execute();
+            }
+        }
+
+        public override void Execute()
+        {
+            MoveToPlayer();
+            Debug.Log("Chasing");
+        }
+
+        public override void Exit()
+        {
+            Debug.Log("Enemy ChaseState sleep");
+        }
     }
 }
