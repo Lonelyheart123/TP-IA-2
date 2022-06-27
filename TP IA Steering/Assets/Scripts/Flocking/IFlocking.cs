7d72f982a7da77022090be9a319c0422febcf6c2
Lonelyheart123 <69641046+Lonelyheart123@users.noreply.github.com>
2022-06-27 19:03:25 -0300

    Version anterior borrada. Version que anda bien subida.

--- TP IA Steering/Assets/Scripts/Flocking/IFlocking.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IFlockingBehaviour
-{
-    Vector3 GetDir(List<Transform> boids, Transform self);
-    float Multiplier { get; set; }
-}
